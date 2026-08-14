bdf66aa3c53aefa147fe8612358c68e051f026e1
reag-dev <34966445+reag-dev@users.noreply.github.com>
2026-08-14 09:21:37 -0300

    Finalizado primeiro exercicio (calculadora de IMC)

--- calculadora-imc.py
+++ calculadora-imc.py
@@ -1,20 +1,33 @@
-import math
-
 print("Calculadora de IMC com base na classificação da OMS")
-weight = float(input("Informe seu peso (KG): "))
-height = float(input("Informe sua altura (M): "))
-imc = (height* height) / weight
 
-if(40 >= imc):
-    print("Obesidade grau III (grave)")
-elif(35.0 >= imc and imc <= 39.9):
-    print("Obesidade grau II (risco grave)")
-elif(30.0 >= imc and imc <= 34.9):
-    print("Obesidade grau I (risco aumentado)")
-elif(25.0 >= imc and imc <= 29.9):
-    print("Sobrepeso (risco moderado)")
-elif(18.5 >= imc and imc <= 24.9):
-    print("Peso normal (adequado)")
-else:
-    print("Magreza (baixo peso)")
+while True:
+    try:
+        weight = float(input("Informe seu peso (KG): "))
+        height = float(input("Informe sua altura (M): "))
+    except ValueError:
+        print("Por favor, forneça um numero de formato válido!")
+        continue
+        
+    if (weight <= 0 or height <= 0):
+        print("Peso e altura deve ser maiores que zero.")
+        continue
 
+    imc =  weight / height ** 2
+
+    print(f"Seu IMC é {imc:.2f}")
+    if imc < 18.5:
+        print("Magreza")
+    elif imc < 25:
+        print("Peso normal")
+    elif imc < 30:
+        print("Sobrepeso")
+    elif imc < 35:
+        print("Obesidade grau I")
+    elif imc < 40:
+        print("Obesidade grau II")
+    else:
+        print("Obesidade grau III")
+    
+    user_command = input("\n Digite [x] para encerrar o programa ou outra tecla para repetir: ")
+    if user_command.lower() == "x":
+        break
